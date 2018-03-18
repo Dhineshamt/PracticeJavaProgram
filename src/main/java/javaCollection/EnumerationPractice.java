02f9b4054e8126eeea60e9decbe51d2a4f24ba95
a0135561 <elivision.11@gmail.com>
2018-03-18 21:33:40 +0530

    Fileupdate

---
+++ src/main/java/javaCollection/EnumerationPractice.java
@@ -0,0 +1,26 @@
+package javaCollection;
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class EnumerationPractice {
+
+	public static void main(String[] args) {
+		
+			Object output = new Object();
+		
+			Vector<Object> element = new Vector<>();
+			element.add('a');
+			element.add(10);
+			element.add("Dhinesh");
+			
+			Enumeration<Object> elements = element.elements();
+			
+			while(elements.hasMoreElements()) {
+				output = elements.nextElement();
+				System.out.println(output);
+			}
+			
+
+	}
+
+}
---
+++ src/main/java/javaCollection/package-info.java
@@ -0,0 +1,8 @@
+/** This package is to explore skill on Java Collection framework
+ * 
+ */
+/**
+ * @author Dhinesh
+ *
+ */
+package javaCollection;
