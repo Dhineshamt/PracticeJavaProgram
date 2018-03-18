85ff649d09803e5f4254aaf7ee4ab7812fccbbd7
a0135561 <elivision.11@gmail.com>
2018-03-18 22:16:28 +0530

    update

--- src/main/java/javaCollection/EnumerationPractice.java
+++ src/main/java/javaCollection/EnumerationPractice.java
@@ -7,17 +7,23 @@ public class EnumerationPractice {
 	public static void main(String[] args) {
 		
 			Object output = new Object();
-		
 			Vector<Object> element = new Vector<>();
 			element.add('a');
 			element.add(10);
 			element.add("Dhinesh");
 			
 			Enumeration<Object> elements = element.elements();
-			
 			while(elements.hasMoreElements()) {
 				output = elements.nextElement();
 				System.out.println(output);
+			}
+			
+			int result;
+			Vector<Integer> numbers = new Vector<>();
+			Enumeration number = numbers.elements();
+			
+			while(number.hasMoreElements()) {
+				result = (int) number.nextElement();
 			}
 			
 
