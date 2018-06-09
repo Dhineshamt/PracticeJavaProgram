b9ce73bb062bdd80c27beb9bcdb19d561e6dc38d
a0135561 <elivision.11@gmail.com>
2018-06-09 16:37:32 +0530

    committing to check git stash

--- src/main/java/stringPrograms/CompareOperatorInString.java
+++ src/main/java/stringPrograms/CompareOperatorInString.java
@@ -5,6 +5,9 @@ public class CompareOperatorInString {
 	public static void main(String[] args) {
 		String name1 = new String("Falcon");
 		String name2 = new String("Falcon");
+		
+		String name3 = "I am Dhinesh";
+		String name4 = "I am Dhinesh";
 
 		if (name1 == name2) {
 			System.out.println("I am Falcon");
@@ -15,6 +18,12 @@ public class CompareOperatorInString {
 			System.out.println("Believe me. I am Falcon");
 		} else
 			System.out.println("Sorry Guys. I am not Falcon");
+		
+		if(name3 == name4) {
+			System.out.println("name3 and name4 are referring to same object since both the varialbes consist same string");
+		} else
+			System.out.println("name3 and name4 are not referring to same object");
+		
 	}
 
 }
