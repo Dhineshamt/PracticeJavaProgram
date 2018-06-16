9904a8dd37185a3bf27d9029d7cc52a43e63573c
Dhinesh <a.dhinesh1@gmail.com>
2018-06-17 00:38:12 +0530

    commit to test user id change in git config

--- src/main/java/practiceJavaProgram/ObjectCasting.java
+++ src/main/java/practiceJavaProgram/ObjectCasting.java
@@ -6,12 +6,12 @@ public class ObjectCasting {
 
 	public static void main(String[] args) {
 
-		 new B().obj();
+		/* new B().obj();
 		objRef.obj();
 		objRef.testMethod();
 		
 		
-		ChromeDriver obj =new ChromeDriver();
+		ChromeDriver obj =new ChromeDriver();*/
 	}
 
 }
